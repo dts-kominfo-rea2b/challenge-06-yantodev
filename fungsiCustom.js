d974ee0538a97190f951d44ebcbd361a7c3de575
yanto <ekocahyanto007@gmail.com>
2022-06-08 14:19:27 +0700

    challange 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -17,8 +18,61 @@ let modifyFile3 = (val) => {
 };
 
 // TODO: Kerjakan bacaData
+const getWord = (words) => {
+  const splitWord = words.split(' ')
+  if (splitWord?.length >= 1) {
+    return splitWord[1]
+  }
+}
+
+const process = (setData) => {
+  const data = JSON.parse(setData)
+  let data2 = '';
+  if (data?.message != undefined) {
+    data2 = data?.message
+  }
+  if (data?.length) {
+    data?.forEach(item => {
+
+      if (item?.message !== undefined) {
+        data2 = item?.message
+      }
+
+      if (item?.data?.message !== undefined) {
+        data2 = item?.data?.message
+      }
+    })
+  }
+  return getWord(data2);
+}
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const fileList = [file1, file2, file3]
+  const result = []
+
+  fileList.forEach(item => {
+    const processItem = new Promise((resolve, reject) => {
+      fs.readFile(item, (error, data) => {
+        // error state
+        if (error) {
+          reject(error)
+          return
+        }
+        // success state
+        const getProcessedItem = process(data)
+        resolve(getProcessedItem)
+      })
+    })
+    result.push(processItem)
+  })
+
+  // call result in callback
+  Promise.all(result).then(values => {
+    fnCallback(null, values)
+  }).catch(error => {
+    fnCallback(error, null)
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
